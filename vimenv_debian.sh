381973bb04a64cde824e5c712095ae8b542f35d0
narikatsu <sukotako@outlook.com>
2021-11-04 19:11:31 -0400

    bug fix. enable x

--- vimenv_debian.sh
+++ vimenv_debian.sh
@@ -5,11 +5,16 @@ sudo apt install git gettext libtinfo-dev libacl1-dev libgpm-dev build-essential
 
 sudo apt install libperl-dev liblua5.2-0 lua5.2 liblua5.2-dev luajit python3-dev ruby-dev
 
+sudo apt install libx11-dev libxtst-dev libxt-dev libsm-dev libxpm-dev
+
 mkdir ~/src
 cd ~/src
 git clone https://github.com/vim/vim.git
-cd vim
-./configure  --prefix=/usr/local/ --with-features=huge --enable-multibyte --enable-gpm --enable-cscope --enable-perlinterp --enable-python3interp --enable-rubyinterp --enable-luainterp --enable-acl --enable-fontset --enable-xim --enable-terminal --enable-fail-if-missing --with-luajit --with-x
+cd ./vim/src
+make distclean
+
+./configure --with-features=huge --enable-perlinterp --enable-python3interp --enable-rubyinterp --enable-luainterp --enable-fail-if-missing --with-luaji --with-x
+
 
 make
 sudo make install
